5469410cfa4a1a06f33ca6c358c2538f9691bc11
Ashrit Yerukola <ashrity@gmail.com>
2021-10-07 17:52:02 -0400

    Triggers next room

---
+++ Assets/Levels/Scripts/Door.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Transform previousRoom;
+    [SerializeField] private Transform nextRoom;
+    [SerializeField] private CameraController cam;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            
+        }
+    }
+}
